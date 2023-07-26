f02df7e037646f77711ebcf171dfbc791f5e252e
shahram <shahramkhaleh2001@gmail.com>
2023-07-27 02:11:05 +0330

    feat: create nav

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
 import { LinearGradient } from "expo-linear-gradient";
 import { SafeAreaView, Text, View } from "react-native";
+import {Bars3CenterLeftIcon, BellIcon} from "react-native-heroicons/solid"
+import { storeColors } from "./src/theme";
 
 export default function App() {
   return (
@@ -7,9 +9,12 @@ export default function App() {
       colors={["rgba(58,131,244,0.4)", "rgba(9,181,211,0.4)"]}
       className="w-full flex-1"
     >
-      <SafeAreaView>
+      <SafeAreaView className="py-8" >
         <View className="container">
-          <Text>Hello World!</Text>
+          <View className="flex-row justify-between items-center px-4" >
+          <Bars3CenterLeftIcon color={storeColors.text} size="30" />
+          <BellIcon color={storeColors.text} size="30" />
+          </View>
         </View>
       </SafeAreaView>
     </LinearGradient>
